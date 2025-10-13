745d8d132bc6f89d995e18cbe517033d7d7d5494
Nathan <n4t10m@gmail.com>
2025-10-13 08:44:12 +0200

    Exercice Analyse de risque

---
+++ Théorie des jeux/exo_analyse_de_risque.py
@@ -0,0 +1,16 @@
+import numpy as np
+from scipy.stats import binom
+
+n = 4
+p = 0.7
+
+risk_failure = 1 - (p ** n)
+
+
+
+risk_failure_check = binom.cdf(3, n, p)
+
+if risk_failure == risk_failure_check:
+    print(f"Probabilité d'échec sur les {n} jours : {risk_failure:.4f}")
+else:
+    print("Le calcul n'est pas bon.")
