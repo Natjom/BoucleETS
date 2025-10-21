4696641de05882b3c13710613b7d4fadf1db4b3d
natjom <n4t10m@gmail.com>
2025-10-21 09:42:41 +0200

    la terre va entrer dans le trou noir et faire un nouveau big bang

--- Arithmétque et Cryptographie/Cesar.py
+++ Arithmétque et Cryptographie/Cesar.py
@@ -11,3 +11,4 @@ if __name__ == '__main__':
     texte_encrypte = "9153787770964"
     texte_decrypte = cesar(texte_encrypte, 4)
     print(f"{texte_decrypte[0:2]}°{texte_decrypte[2:4]}'{texte_decrypte[4:6]}.{texte_decrypte[6]}\"N {texte_decrypte[6:8]}°{texte_decrypte[8:10]}'{texte_decrypte[10:12]}.{texte_decrypte[12]}\"W")
+
