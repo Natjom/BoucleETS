d8f4b45241443952341bcebc776b8cb0cddb18e2
natjom <n4t10m@gmail.com>
2025-10-21 09:46:49 +0200

    la terre va entrer dans le trou noir et faire un nouveau big bang

--- Arithmétque et Cryptographie/Cesar en 2 lignes.py
+++ Arithmétque et Cryptographie/Cesar en 2 lignes.py
@@ -1,4 +1,2 @@
 texte_decrypte = (''.join(map(str, [str((int(e) + 4) % 10) for e in "9153787770964"])))
 print(f"{texte_decrypte[0:2]}°{texte_decrypte[2:4]}'{texte_decrypte[4:6]}.{texte_decrypte[6]}\"N {texte_decrypte[6:8]}°{texte_decrypte[8:10]}'{texte_decrypte[10:12]}.{texte_decrypte[12]}\"W")
-
-
